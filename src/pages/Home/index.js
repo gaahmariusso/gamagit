977672bf9529e64d76e5945c12425b55e6648e10
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2021-07-16 00:07:23 -0300

    styled Home / Repositories

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,41 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import * as S from './styled';
+import { useHistory } from 'react-router-dom';
 
 function App(props) {
+    const history = useHistory();
     const [user, setUser] = useState('');
+    const [error, setError] = useState(false);
 
     function handleSearch() {
         axios.get(`https://api.github.com/users/${user}/repos`)
-            .then(response => console.log(response.data));
+            .then(response => {
+                const repositories = response.data;
+                const repositoriesName = [];
+                repositories.map((repository) => {
+                    repositoriesName.push(repository.name);
+                });
+                localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+                setError(false);
+                history.push('/repositories');
+            })
+            .catch(err => {
+                setError(true);
+            });
     }
 
     return (
-        <S.Container>
-            <S.Input className="userInput" placeholder="Usuário" value={user} onChange={e => setUser(e.target.value)} />
-            <S.Button type="button" onClick={handleSearch}>Pesquisar</S.Button>
-        </S.Container>
+        <S.HomeContainer>
+            <S.Content>
+                <S.Input className="userInput" placeholder="Usuário" value={user} onChange={e => setUser(e.target.value)} />
+                <S.Button type="button" onClick={handleSearch}>Pesquisar</S.Button>
+            </S.Content>
+            {error
+                ? <S.errorMessage>Ocorreu um erro. Tente Novamente !</S.errorMessage>
+                : ''
+            }
+        </S.HomeContainer>
     );
 }
 
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+
 import * as S from './styled';
 
-export default function respositories() {
+export default function Repositories() {
+    const history = useHistory();
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        if (repositoriesName !== null) {
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            history.push('/');
+        }
+    }, []);
+
     return (
         <S.Container>
-            <S.Title>Repositories</S.Title>
+            <S.Title>Repositórios</S.Title>
             <S.List>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório</S.ListItem>
+                {repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositório: {repository}</S.ListItem>
+                    )
+                })}
             </S.List>
+            <S.LinkHome to="/">Voltar</S.LinkHome>
         </S.Container>
     )
 }
